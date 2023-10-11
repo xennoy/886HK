5310829a64fdb7fde30c77a21e8badff6f5ddc6e
xennoy <me@rickchan.com>
2023-10-11 04:29:05 +0100

    Add files via upload

--- Archive/api/order/controllers/order.js
+++ Archive/api/order/controllers/order.js
@@ -38,7 +38,6 @@ module.exports = createCoreController('api::order.order', ({ strapi }) => ({
         }
 
         ctx.query.filters = {}
-        ctx.query.filters.order_date = {}
         if(!(ctx.query.time_period === undefined || ctx.query.time_period === null || ctx.query.time_period === '')) {
             var currentDate = new Date()
             currentDate.setDate(currentDate.getDate() - parseInt(ctx.query.time_period))
@@ -47,13 +46,14 @@ module.exports = createCoreController('api::order.order', ({ strapi }) => ({
             let year = currentDate.getFullYear();   
             var period_date = year + '-' + month + '-' + day
 
-            ctx.query.filters.order_date.$gte = period_date
+            ctx.query.filters.order_date = {$gte: period_date}
         } else {
+            ctx.query.filters.order_date = {}
             if(!(ctx.query.startDate === undefined || ctx.query.startDate === null || ctx.query.startDate === '')) {
                 ctx.query.filters.order_date.$gte = ctx.query.startDate
             }
             if(!(ctx.query.endDate === undefined || ctx.query.endDate === null || ctx.query.endDate === '')) {
-                ctx.query.filters.order_date.$lt = ctx.query.endDate
+                ctx.query.filters.order_date.$lte = ctx.query.endDate
             }
         }
 
@@ -392,7 +392,6 @@ module.exports = createCoreController('api::order.order', ({ strapi }) => ({
         // console.log(ctx.query)
 
         ctx.query.filters = {}
-        ctx.query.filters.order_date = {}
         if(!(ctx.query.time_period === undefined || ctx.query.time_period === null || ctx.query.time_period === '')) {
             var currentDate = new Date()
             currentDate.setDate(currentDate.getDate() - parseInt(ctx.query.time_period))
@@ -401,13 +400,14 @@ module.exports = createCoreController('api::order.order', ({ strapi }) => ({
             let year = currentDate.getFullYear();   
             var period_date = year + '-' + month + '-' + day
 
-            ctx.query.filters.order_date.$gte = period_date
+            ctx.query.filters.order_date = {$gte: period_date}
         } else {
+            ctx.query.filters.order_date = {}
             if(!(ctx.query.startDate === undefined || ctx.query.startDate === null || ctx.query.startDate === '')) {
                 ctx.query.filters.order_date.$gte = ctx.query.startDate
             }
             if(!(ctx.query.endDate === undefined || ctx.query.endDate === null || ctx.query.endDate === '')) {
-                ctx.query.filters.order_date.$lt = ctx.query.endDate
+                ctx.query.filters.order_date.$lte = ctx.query.endDate
             }
         }
 
